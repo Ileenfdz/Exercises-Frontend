36152b34830f1096206226185627499732d9b939
Ileenfdz <ileenfdz@gmail.com>
2022-01-31 19:46:54 +0100

    Added test succesfully

--- FizzBuzz-JS/src/scripts/Example.js
+++ FizzBuzz-JS/src/scripts/Example.js
@@ -1,3 +1,6 @@
-export function sum( a, b) {
-    return a + b;
+export function fizzBuzz(a) {
+    let array = a.toString().split('');
+
+    return (a % 3 == 0 || array.find(i => i == 3)?'fizz':a % 5 == 0 || array.find(i => i == 5)?'buzz':a);
 }
+// export function fizzBuzz(a) { return (a % 3 == 0?'fizz':a % 5 == 0?'buzz':a)}
--- FizzBuzz-JS/tests/example.test.js
+++ FizzBuzz-JS/tests/example.test.js
@@ -1,7 +1,27 @@
-import { sum } from "../src/scripts/Example";
+import { fizzBuzz } from "../src/scripts/Example";
 
 describe('Just an example to testing jest installation', () => {
+    test('should be buzz', () => {
+        expect(fizzBuzz(5)).toBe('buzz');
+    });
+
+    test('should be fizz', () => {
+        expect(fizzBuzz(3)).toBe('fizz');
+    });
+
     test('should be two', () => {
-        expect(sum(1,1)).toBe(2);
+        expect(fizzBuzz(2)).toBe(2);
+    });
+
+    test('should be two', () => {
+        expect(fizzBuzz(2)).toBe(2);
+    });
+    
+    test('should be fizz', () => {
+        expect(fizzBuzz(31)).toBe('fizz');
+    });
+
+    test('should be buzz', () => {
+        expect(fizzBuzz(52)).toBe('buzz');
     });
 });
